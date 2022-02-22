3d7657e43acca5e0d28c350ca94f49c4cd55295d
shaikmohammed238 <shaikmohammedghoue238@gmail.com>
2022-02-22 22:21:37 +0530

    added register api

---
+++ BussinessLayer/Interfaces/IUserBL.cs
@@ -0,0 +1,13 @@
+﻿using CommonLayer.Models;
+using RepositoryLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BussinessLayer.Interfaces
+{
+    public interface IUserBL
+    {
+        public User Registration(UserRegModel userRegModel);
+    }
+}
---
+++ BussinessLayer/Services/UserBL.cs
@@ -0,0 +1,31 @@
+﻿using BussinessLayer.Interfaces;
+using CommonLayer.Models;
+using RepositoryLayer.Entities;
+using RepositoryLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BussinessLayer.Services
+{
+    public class UserBL : IUserBL
+    {
+        private readonly IUserRL userRL;
+        public UserBL(IUserRL userRL)
+        {
+            this.userRL = userRL;
+        }
+        public User Registration(UserRegModel userRegModel)
+        {
+            try
+            {
+                return userRL.Registration(userRegModel);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
