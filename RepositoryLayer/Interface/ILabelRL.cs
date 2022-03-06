e08d4de2f7873ed5fc5d0c102ec4d52d51a9cf8b
shaikmohammed238 <shaikmohammedghoue238@gmail.com>
2022-03-06 18:20:19 +0530

    added label all part

--- RepositoryLayer/Interface/ILabelRL.cs
+++ RepositoryLayer/Interface/ILabelRL.cs
@@ -10,5 +10,13 @@ namespace RepositoryLayer.Interface
     {
         //interface of the repository layer for create label
         public string CreateLabel(LabelModel labelModel);
+        //interface of the repository layer for get all label
+        public IEnumerable<Label> GetAllLabels(long userId);
+        //interface of the repository layer for get id label
+        public IEnumerable<Label> GetId(long labelId);
+        //interface of the repository layer for update label
+        public string Update(LabelModel labelModel, long labelId);
+        //interface of the repository layer for delete label
+        public string DeleteLabel(long labelId);
     }
 }
